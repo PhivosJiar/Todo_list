b6aea9f5fe6727c333622ada9bd021aa1108bdad
邱 柏源 <paulyuancho@gmail.com>
2021-01-05 22:35:32 +0800

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+   
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    </head>
+    <body >
+    <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+  <!-- Navbar content -->
+        </nav>
+        <div id="app">
+            <app> </app>
+        
+        </div>
+
+
+    </body>
+
+    <script src="{{ mix('js/app.js') }}"></script>
+</html>
